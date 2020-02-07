006b44f92e90291ab6595204424d84205c6bedb4
Nathan Taylor <nbtaylor@gmail.com>
2020-02-07 15:35:24 -0700

    wip

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,13 +5,7 @@ import cors from "cors";
 const app = express();
 
 app.set("port", process.env.PORT || 3001);
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "../views"));
-
 app.use(cors);
-app.use(
-    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
-);
 
 // routes
 import * as routes from "./routes";
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import app from "./app";
 
 const server = app.listen(app.get("port"), () => {
-    console.log("Listening on port %d", app.get("port"));
+    console.log("Backend listening on port %d", app.get("port"));
 });
