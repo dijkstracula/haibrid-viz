01c38955d67daedd6786b95eb1b2beb995950ce8
Nathan Taylor <nbtaylor@gmail.com>
2020-02-07 21:59:06 -0700

    argh

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import * as sample from './sample';
 
 type AppState = {
   msg: String
+  sample: sample.Sample | undefined;
 }
 
 const URL = "ws://localhost:3030";
 
 export default class App extends Component<{}, AppState> {
-  public state = { msg: "Conncting to " + URL + "..."};
+  public state: AppState = {sample: undefined, msg: "Conncting to " + URL + "..."};
   ws = new WebSocket(URL);
 
   componentDidMount() {
@@ -17,10 +19,11 @@ export default class App extends Component<{}, AppState> {
     };
     this.ws.onmessage = (ev: MessageEvent) => {
       const blob = JSON.parse(ev.data);
-      this.setState({msg: blob["msg"]})
+      const s = blob["sample"] as sample.Sample;
+      this.setState({msg: blob["msg"], sample: s as sample.Sample});
     };
     this.ws.onclose = () => {
-      this.setState({msg: "Reconnecting to " + URL})
+      this.setState({msg: "Reconnecting to " + URL, sample: undefined})
       this.ws = new WebSocket(URL)
     };
     this.ws.onerror = (ev: Event) => {
@@ -30,7 +33,8 @@ export default class App extends Component<{}, AppState> {
   render() {
     return (
       <div className="App">
-        {this.state.msg}
+        <h1>{this.state.msg}</h1>
+        <p>latency: {this.state.sample !== undefined ? (this.state.sample as sample.Sample).lat : "N/A"} </p>
       </div>
     )
   }
