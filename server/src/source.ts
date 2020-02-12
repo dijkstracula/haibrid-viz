fcd217985aac681ab0b622aacca8a876735bf52e
Nathan Taylor <nbtaylor@gmail.com>
2020-02-12 01:50:51 -0700

    canned data straight from sandwich-load-gen.py

--- server/src/source.ts
+++ server/src/source.ts
@@ -1,26 +1,28 @@
 // A source emits samples.
 import * as fs from "fs";
 import EventEmitter from "events";
-import * as sample from "./sample";
+import {Sample} from "./sample";
 
 class CannedSource {
-    public samples: sample.Sample[]
+    public samples: Sample[]
     idx: number
 
     constructor(path: string) {
-        this.samples = JSON.parse(fs.readFileSync(path, "utf8"));
+        this.samples = JSON.parse(fs.readFileSync(path, "utf8"))["runs"]["split"] as Sample[];
         this.idx = 0;
         
         setInterval(() => {
+            console.log(this.idx);
             this.idx = (this.idx + 1) % this.samples.length;
         }, 99);
     }
 
-    on(event: "message", f: (sample: sample.Sample) => void) {
+    on(event: "message", f: (sample: Sample) => void) {
         setInterval(() => {
             if (this.samples.length === 0) {
                 return;
             }
+            console.log(this.samples[this.idx]);
             f(this.samples[this.idx]);
         }, 99);      
     }
