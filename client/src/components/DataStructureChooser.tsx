7b37a4ca3db2bd53691f686e724c5ae7a0c6ae6f
Nathan Taylor <v-natay@microsoft.com>
2020-02-28 17:48:13 -0500

    add DataStructureChooser

---
+++ client/src/components/DataStructureChooser.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import * as d3 from 'd3';
+
+interface Props {
+    structures: string[]
+    actives: string[]
+    onChange: (actives: string[]) => void
+}
+
+function inputsFromProps(props: Props): JSX.Element[] {
+    return props.structures.map((s: string) => {
+        const checked = (props.actives.find((a) => a === s) !== undefined)
+        return (
+            <label>
+                {s}
+                <input 
+                    type="checkbox"
+                    checked={checked}
+                >
+                </input>
+            </label>)
+    })
+}
+export const DataStructureChooser = (props: Props) => {
+    return (
+        <div>
+            {inputsFromProps(props)}
+        </div>
+    )
+}
